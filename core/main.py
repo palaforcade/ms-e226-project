aabe5fa894ad7ca1a0ccbdd083a223c15717021d
Pierre-Amaury Laforcade <pa.laforcade@gmail.com>
2023-11-04 17:29:09 -0700

    :fire: Remove code after dataset change

--- core/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from constants.columns import COLUMNS_SUBSET
-
-
-raw_data = pd.read_csv("../data/raw_dataset.csv")
-
-data_subset = raw_data[COLUMNS_SUBSET]
-
-print(data_subset.head())
