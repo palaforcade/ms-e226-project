84abe8c0c96be76ce0b648fdcf41432155408bea
Pierre-Amaury Laforcade <pa.laforcade@gmail.com>
2023-11-06 17:20:36 -0800

    :sparkles: Add column selection to preprocessing

--- core/preprocessing/main.py
+++ core/preprocessing/main.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 
+from constants.columns import DatasetColumns
+
 
 class Preprocessor:
     def __init__(self, data_folder):
@@ -24,9 +26,24 @@ class Preprocessor:
             index=False,
         )
 
+    def select_covariates(self):
+        self.preprocessed_dataset = self.raw_dataset_without_holdout[
+            [col.value for col in DatasetColumns]
+        ]
+
+    def export_preprocessed_dataset(self):
+        self.preprocessed_dataset.to_csv(
+            os.path.join(self.data_folder, "preprocessed/preprocessed_dataset.csv"),
+            index=False,
+        )
+
     def run_preprocessing(self):
         """
         Run the preprocessing pipeline
         """
 
         self.create_holdout_set()
+
+        self.select_covariates()
+
+        self.export_preprocessed_dataset()
