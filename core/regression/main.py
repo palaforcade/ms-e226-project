9b71c41b9fdc3d2d6375bcf64c651884b5c88d00
Axeldallize <axelda75@gmail.com>
2023-11-08 15:48:16 -0800

    created code for lambda selection

--- core/regression/main.py
+++ core/regression/main.py
@@ -2,6 +2,7 @@ import logging
 
 from regression.ols_model import OLSBaselineModel
 from regression.lasso_with_squares import LassoWithSquaresModel
+from regression.lasso_lambda_selection import LassoLambdaSearch, LAMBDA_RANGE
 from regression.neural_networks.perceptron import Perceptron
 from regression.neural_networks.one_layer_model import OneLayerModel
 from regression.neural_networks.multi_layer_model import MultiLayerModel
@@ -18,6 +19,8 @@ def run_regression_models(data_folder):
     OLSBaselineModel(data_folder).compute_test_mse()
 
     LassoWithSquaresModel(data_folder).compute_test_mse()
+
+    LassoLambdaSearch(data_folder, LAMBDA_RANGE)
 
     Perceptron(
         data_folder=data_folder, model_structure=OneLayerModel
