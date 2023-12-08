a7ec7a151801bcaed527159d1eac313eb7095b32
Pierre-Amaury Laforcade <pa.laforcade@gmail.com>
2023-12-07 21:55:50 -0800

    :recycle: Use statsmodels to do the OLS regression rather than sklear

--- core/regression/ols_model.py
+++ core/regression/ols_model.py
@@ -1,14 +1,11 @@
 import os
+import pandas as pd
 import numpy as np
 import logging
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from scipy.stats import t
-from torch import cov
+import statsmodels.api as sm
 
 from constants.columns import DatasetColumns
 from constants.seed import RANDOM_SEED
-
 
 TRAIN_SPLIT_RATIO = 0.7
 
@@ -36,57 +33,41 @@ class OLSBaselineModel:
 
     def train_model(self):
         """
-        Train the model
+        Train the model using statsmodels
         """
-
         covariates = self.train_set.drop(columns=[DatasetColumns.REDSHIFT.value])
         outcomes = self.train_set[DatasetColumns.REDSHIFT.value]
 
-        self.model = LinearRegression().fit(covariates, outcomes)
+        # Adding a constant for the intercept term
+        covariates = sm.add_constant(covariates)
+
+        self.model = sm.OLS(outcomes, np.asarray(covariates, dtype=float)).fit()
 
         # Log the MSE on the train set (in-sample error)
-        self.train_mse = ((self.model.predict(covariates) - outcomes) ** 2).mean()
+        predictions = self.model.predict(covariates)
+        self.train_mse = ((predictions - outcomes) ** 2).mean()
         logger.info(f"Train MSE for OLS baseline model: {self.train_mse}")
 
     def compute_test_mse(self):
         """
         Compute the MSE on the test set
         """
-
         covariates = self.test_set.drop(columns=[DatasetColumns.REDSHIFT.value])
         outcomes = self.test_set[DatasetColumns.REDSHIFT.value]
 
-        self.test_mse = ((self.model.predict(covariates) - outcomes) ** 2).mean()
+        # Adding a constant for the intercept term
+        covariates = sm.add_constant(covariates)
+
+        predictions = self.model.predict(covariates)
+        self.test_mse = ((predictions - outcomes) ** 2).mean()
 
         logger.info(f"Test MSE for OLS baseline model: {self.test_mse}")
 
     def compute_coefficient_p_values(self):
         """
         Compute the p-values for all the linear regression coefficients
+        using statsmodels
         """
-
-        covariates = (
-            self.train_set.drop(columns=[DatasetColumns.REDSHIFT.value])
-            .to_numpy()
-            .astype(np.float64)
-        )
-
-        outcomes = self.train_set[DatasetColumns.REDSHIFT.value].to_numpy()
-
-        n = len(self.train_set)
-        p = covariates.shape[1]
-        dof = n - p - 1
-
-        # Compute the standard errors of the coefficients
-        residuals = outcomes - self.model.predict(covariates)
-        mse = (residuals**2).sum() / dof
-        prod_matrix = np.dot(covariates.T, covariates)
-        cov_matrix = mse * np.linalg.inv(prod_matrix)
-
-        se = np.diag(cov_matrix) ** 0.5
-
-        # Compute the t-statistics and p-values
-        t_values = self.model.coef_ / se
-        p_values = (1 - t.cdf(abs(t_values), dof)) * 2
-
-        return p_values
+        # The p-values are directly available in the summary
+        self.p_values = self.model.summary2().tables[1]["P>|t|"]
+        return self.p_values
